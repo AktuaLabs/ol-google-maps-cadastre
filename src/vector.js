8144d771336e831ac24c34cebfa9a42cfad7b2bd
Adrián Ríos <adrian.web.dev@gmail.com>
2017-05-04 18:04:35 +0200

    allow to search by cadastral reference

--- src/vector.js
+++ src/vector.js
@@ -1,17 +1,26 @@
 import bootstrap from 'bootstrap-webpack';
-import jQuery from 'jquery';
+import $ from 'jquery';
 import * as cadaster from '../lib/cadaster';
 
 // Coords are for Madrid in EPSG:3857 Projection
 const initialCenter = [-413524.323023, 4925754.976753];
-const map = cadaster.GMap.init(initialCenter, 19);
+const map = cadaster.GMap.init(initialCenter);
 
 cadaster.Cadaster.init(map);
 
-cadaster.Cadaster.fetchFeature('0484061WF5708S0001SO', (feature) => {
-    cadaster.GMap.addVectorLayer(feature.coords);
-    cadaster.GMap.setCenter(feature.center);
+$('#search-form').on('submit', function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+
+    const reference = $(this).find('[name=refcat]').val();
+    cadaster.Cadaster.fetchFeature(reference, (feature) => {
+        cadaster.GMap.addVectorLayer(feature.coords);
+        cadaster.GMap.setCenter(feature.center);
+        cadaster.GMap.setZoom(18);
+    });
 });
+
+
 
 
 
