977dbf1f614c500a297ed3e5d45560ee2cf584f3
Adrián Ríos <adrian.web.dev@gmail.com>
2017-05-04 18:43:52 +0200

    split cadaster lib

--- src/vector.js
+++ src/vector.js
@@ -1,22 +1,21 @@
 import bootstrap from 'bootstrap-webpack';
 import $ from 'jquery';
-import * as cadaster from '../lib/cadaster';
+import cadaster from '../lib/cadaster';
+import gmap from '../lib/gmap';
 
 // Coords are for Madrid in EPSG:3857 Projection
 const initialCenter = [-413524.323023, 4925754.976753];
-const map = cadaster.GMap.init(initialCenter);
-
-cadaster.Cadaster.init(map);
+const map = gmap.init('vector-map', initialCenter);
 
 $('#search-form').on('submit', function (e) {
     e.preventDefault();
     e.stopImmediatePropagation();
 
     const reference = $(this).find('[name=refcat]').val();
-    cadaster.Cadaster.fetchFeature(reference, (feature) => {
-        cadaster.GMap.addVectorLayer(feature.coords);
-        cadaster.GMap.setCenter(feature.center);
-        cadaster.GMap.setZoom(18);
+    cadaster.fetchFeature(reference, (feature) => {
+        gmap.addVectorLayer(feature.coords);
+        gmap.setCenter(feature.center);
+        gmap.setZoom(16);
     });
 });
 
